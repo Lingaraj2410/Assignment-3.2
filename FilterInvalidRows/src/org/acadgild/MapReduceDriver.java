055deaa0e986304ad30e5264d17ba0c91799fadd
Lingaraj A J <lingaraj2410@gmail.com>
2017-12-01 00:21:23 +0530

    Update MapReduceDriver.java

--- FilterInvalidRows/src/org/acadgild/MapReduceDriver.java
+++ FilterInvalidRows/src/org/acadgild/MapReduceDriver.java
@@ -1,16 +1,11 @@
 package org.acadgild;
 
 import org.apache.hadoop.conf.Configuration;
-
 import org.apache.hadoop.fs.Path;
-
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-
 import org.apache.hadoop.mapreduce.Job;
-
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class MapReduceDriver 
@@ -21,24 +16,17 @@ public class MapReduceDriver
 	{
 	
 	Configuration conf = new Configuration();
+	
+	Job job = new Job(conf, "DataValidation");
+    	job.setJarByClass(MapReduceDriver.class);
+    	job.setMapperClass(MapperClass.class);
+	job.setNumReduceTasks(0);
+	job.setOutputKeyClass(NullWritable.class);
+	job.setOutputValueClass(Text.class);
+	FileInputFormat.addInputPath(job, new Path(args[0]));
+	FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-    Job job = new Job(conf, "DataValidation");
-
-    job.setJarByClass(MapReduceDriver.class);
-
-    job.setMapperClass(MapperClass.class);
-    
-    job.setNumReduceTasks(0);
-
-    job.setOutputKeyClass(NullWritable.class);
-
-    job.setOutputValueClass(Text.class);
-
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
+    	System.exit(job.waitForCompletion(true) ? 0 : 1);
 
 	}
 
